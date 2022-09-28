4644965144550543ce80ad09af251b7a73fbd6c9
Pragati121 <Pragati@infostride.com>
2022-09-28 20:43:07 +0530

    Nop commerce test cases2

---
+++ src/test/java/FeatureFile/Electronic.feature
@@ -0,0 +1,7 @@
+Feature: Login Action
+  Scenario:Add Eletronics items
+    Given User is on the Home Page
+    When  click on electronics
+    And   add product
+    And   go to shopping cart page
+    And   click on checkout
--- src/test/java/Pages/PageFactory.java
+++ src/test/java/Pages/PageFactory.java
@@ -5,6 +5,11 @@ public class PageFactory
 {
    private HomePage Home;
   private HomePage Homemethod12;
+    private ElectronicPage Electronic;
+    private ElectronicPage Electronics23;
+
+    private ElectronicPage electronics;
+    private ElectronicPage electronicsUser;
 
     WebDriver driver;
 
@@ -24,6 +29,30 @@ public class PageFactory
           Homemethod12 = new HomePage(driver);
        }
         return Homemethod12;
+    }
+    public ElectronicPage getElectronic() {
+        if (Electronic == null) {
+            Electronic= new ElectronicPage(driver);
+        }
+        return Electronic;
+    }
+    public ElectronicPage getElectronic23() {
+        if (Electronics23 == null) {
+            Electronics23= new ElectronicPage(driver);
+        }
+        return Electronics23;
+    }
+    public ElectronicPage getelectronics() {
+        if (electronics == null) {
+            electronics= new ElectronicPage(driver);
+        }
+        return electronics;
+    }
+    public ElectronicPage getElectronicsUser() {
+        if (electronicsUser == null) {
+            electronicsUser= new ElectronicPage(driver);
+        }
+        return electronicsUser;
     }
     }
 
